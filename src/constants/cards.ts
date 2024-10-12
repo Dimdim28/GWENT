b5df5a5c36b31e6d4a726d3c0de95faf2370990e
Dimdim28 <wwwdimanes1@gmail.com>
2024-10-12 20:01:13 +0300

    feat: added images for all fractions

--- src/constants/cards.ts
+++ src/constants/cards.ts
@@ -1,40 +1,750 @@
-import harpia from '../assets/cards-animated/Harpia_Premium.webp';
-import mahakam from '../assets/cards-animated/redman.gif';
-import regis from '../assets/cards-animated/regis.gif';
-import skellige_warrior from '../assets/cards-animated/skellige_warrior.gif';
-import tissaya from '../assets/cards-animated/tissaya.gif';
+import elfs_brovven from '../assets/cards-static/elfs_brovven.jpg';
+import elfs_dava from '../assets/cards-static/elfs_dava.jpg';
+import elfs_dolblatanna from '../assets/cards-static/elfs_dolblatanna.jpg';
+import elfs_driada from '../assets/cards-static/elfs_driada.jpg';
+import elfs_findabair from '../assets/cards-static/elfs_findabair.jpg';
+import elfs_mahakam from '../assets/cards-static/elfs_mahakam.jpg';
+import elfs_milva from '../assets/cards-static/elfs_milva.jpg';
+import elfs_oackritters from '../assets/cards-static/elfs_oackritters.jpg';
+import elfs_saskiya from '../assets/cards-static/elfs_saskiya.jpg';
+import elfs_sheldon from '../assets/cards-static/elfs_sheldon.jpg';
+import elfs_shirru from '../assets/cards-static/elfs_shirru.jpg';
+import elfs_skrimmeyster from '../assets/cards-static/elfs_skrimmeyster.jpg';
+import elfs_sukkub from '../assets/cards-static/elfs_sukkub.jpg';
+import elfs_swordmaster from '../assets/cards-static/elfs_swordmaster.jpg';
+import elfs_vernosiell from '../assets/cards-static/elfs_vernosiell.jpg';
+import elfs_volounteer from '../assets/cards-static/elfs_volounteer.jpg';
+import elfs_vrihhed from '../assets/cards-static/elfs_vrihhed.jpg';
+import elfs_yarpen from '../assets/cards-static/elfs_yarpen.jpg';
+import elfs_yorvet from '../assets/cards-static/elfs_yorvet.jpg';
+import elfs_zoltan from '../assets/cards-static/elfs_zoltan.jpg';
+import monster_alp from '../assets/cards-static/monster_alp.jpg';
+import monster_arianna from '../assets/cards-static/monster_arianna.jpg';
+import monster_auberon from '../assets/cards-static/monster_auberon.jpg';
+import monster_bargest from '../assets/cards-static/monster_bargest.jpg';
+import monster_detlaph from '../assets/cards-static/monster_detlaph.jpg';
+import monster_eredin from '../assets/cards-static/monster_eredin.jpg';
+import monster_foglet from '../assets/cards-static/monster_foglet.jpg';
+import monster_goliafr from '../assets/cards-static/monster_goliaf.jpg';
+import monster_griphon from '../assets/cards-static/monster_griphon.jpg';
+import monster_grot from '../assets/cards-static/monster_grot.jpg';
+import monster_harpiya from '../assets/cards-static/monster_harpiya.jpg';
+import monster_imlerikh from '../assets/cards-static/monster_imlerikh.jpg';
+import monster_keyran from '../assets/cards-static/monster_keyran.jpg';
+import monster_nacker from '../assets/cards-static/monster_nacker.jpg';
+import monster_nightwreith from '../assets/cards-static/monster_nightwreith.jpg';
+import monster_nithraal from '../assets/cards-static/monster_nithraal.jpg';
+import monster_skolopendromorph from '../assets/cards-static/monster_skolopendromorph.jpg';
+import monster_viy from '../assets/cards-static/monster_viy.jpg';
+import monster_wildhuntwarrior from '../assets/cards-static/monster_wildhuntwarrior.jpg';
+import monster_zhagnitca from '../assets/cards-static/monster_zhagnitca.jpg';
+import nilfgaard_alba from '../assets/cards-static/nilfgaard_alba.jpg';
+import nilfgaard_assire from '../assets/cards-static/nilfgaard_assire.jpg';
+import nilfgaard_catapult from '../assets/cards-static/nilfgaard_catapult.jpg';
+import nilfgaard_cavalerry from '../assets/cards-static/nilfgaard_cavalerry.jpg';
+import nilfgaard_damien from '../assets/cards-static/nilfgaard_damien.jpg';
+import nilfgaard_emgyr from '../assets/cards-static/nilfgaard_emgyr.jpg';
+import nilfgaard_fringiliya from '../assets/cards-static/nilfgaard_fringiliya.jpg';
+import nilfgaard_impera from '../assets/cards-static/nilfgaard_impera.jpg';
+import nilfgaard_inf from '../assets/cards-static/nilfgaard_inf.jpg';
+import nilfgaard_killer from '../assets/cards-static/nilfgaard_killer.jpg';
+import nilfgaard_peyrak from '../assets/cards-static/nilfgaard_peyrak.jpg';
+import nilfgaard_poison from '../assets/cards-static/nilfgaard_poison.jpg';
+import nilfgaard_riens from '../assets/cards-static/nilfgaard_riens.jpg';
+import nilfgaard_search from '../assets/cards-static/nilfgaard_search.jpg';
+import nilfgaard_shield from '../assets/cards-static/nilfgaard_shield.jpg';
+import nilfgaard_skellen from '../assets/cards-static/nilfgaard_skellen.jpg';
+import nilfgaard_strong_cav from '../assets/cards-static/nilfgaard_strong_cav.jpg';
+import nilfgaard_terranova from '../assets/cards-static/nilfgaard_terranova.jpg';
+import nilfgaard_trainer from '../assets/cards-static/nilfgaard_trainer.jpg';
+import nilfgaard_vilgeforc from '../assets/cards-static/nilfgaard_vilgeforc.jpg';
+import north_bianka from '../assets/cards-static/north_bianka.jpg';
+import north_bluestripes from '../assets/cards-static/north_bluestripes.jpg';
+import north_cintraknight from '../assets/cards-static/north_cintraknight.jpg';
+import north_demavend from '../assets/cards-static/north_demavend.jpg';
+import north_detmold from '../assets/cards-static/north_detmold.jpg';
+import north_diycstra from '../assets/cards-static/north_diycstra.jpg';
+import north_draug from '../assets/cards-static/north_draug.jpg';
+import north_filippa from '../assets/cards-static/north_filippa.jpg';
+import north_foltest from '../assets/cards-static/north_foltest.jpg';
+import north_hencelt from '../assets/cards-static/north_hencelt.jpg';
+import north_inf from '../assets/cards-static/north_inf.jpg';
+import north_kaedvencav from '../assets/cards-static/north_kaedvencav.jpg';
+import north_meva from '../assets/cards-static/north_meva.jpg';
+import north_radovid from '../assets/cards-static/north_radovid.jpg';
+import north_redanarch from '../assets/cards-static/north_redanarch.jpg';
+import north_roshe from '../assets/cards-static/north_roshe.jpg';
+import north_shani from '../assets/cards-static/north_shani.jpg';
+import north_sheala from '../assets/cards-static/north_sheala.jpg';
+import north_tisaya from '../assets/cards-static/north_tisaya.jpg';
+import north_yan from '../assets/cards-static/north_yan.jpg';
+import skellige_ankrait from '../assets/cards-static/skellige_ankrait.jpg';
+import skellige_ankraitwarrrior from '../assets/cards-static/skellige_ankraitwarrrior.jpg';
+import skellige_bear from '../assets/cards-static/skellige_bear.jpg';
+import skellige_berserk from '../assets/cards-static/skellige_berserk.jpg';
+import skellige_brideofsea from '../assets/cards-static/skellige_brideofsea.jpg';
+import skellige_crow from '../assets/cards-static/skellige_crow.jpg';
+import skellige_drakkar from '../assets/cards-static/skellige_drakkar.jpg';
+import skellige_druid from '../assets/cards-static/skellige_druid.jpg';
+import skellige_drummond from '../assets/cards-static/skellige_drummond.jpg';
+import skellige_hemdal from '../assets/cards-static/skellige_hemdal.jpg';
+import skellige_hyalmar from '../assets/cards-static/skellige_hyalmar.jpg';
+import skellige_keris from '../assets/cards-static/skellige_keris.jpg';
+import skellige_koral from '../assets/cards-static/skellige_koral.jpg';
+import skellige_lugos from '../assets/cards-static/skellige_lugos.jpg';
+import skellige_melluzine from '../assets/cards-static/skellige_melluzine.jpg';
+import skellige_svalblod from '../assets/cards-static/skellige_svalblod.jpg';
+import skellige_svalblodwarrior from '../assets/cards-static/skellige_svalblodwarrior.jpg';
+import skellige_threeeyes from '../assets/cards-static/skellige_threeeyes.jpg';
+import skellige_tiershach from '../assets/cards-static/skellige_tiershach.jpg';
+import skellige_tyr from '../assets/cards-static/skellige_tyr.jpg';
 
 import { Card } from './../types/general';
 
-export const ALL_CARDS: Card[] = [
+// export const ALL_CARDS: Card[] = [
+//   {
+//     fraction: 'skellige',
+//     cost: 9,
+//     value: 5,
+//     image: skellige_warrior,
+//   },
+//   {
+//     fraction: 'monster',
+//     cost: 0,
+//     value: 6,
+//     image: harpia,
+//   },
+//   {
+//     fraction: 'north',
+//     cost: 2,
+//     value: 8,
+//     image: regis,
+//   },
+//   {
+//     fraction: 'elf',
+//     cost: 5,
+//     value: 7,
+//     image: mahakam,
+//   },
+//   {
+//     fraction: 'nilfgaard',
+//     cost: 2,
+//     value: 17,
+//     image: tissaya,
+//   },
+// ];
+
+export const skelligeCards: Card[] = [
+  {
+    fraction: 'skellige',
+    cost: 5,
+    value: 3,
+    image: skellige_ankrait,
+  },
+  {
+    fraction: 'skellige',
+    cost: 7,
+    value: 4,
+    image: skellige_ankraitwarrrior,
+  },
+  {
+    fraction: 'skellige',
+    cost: 4,
+    value: 2,
+    image: skellige_bear,
+  },
+  {
+    fraction: 'skellige',
+    cost: 6,
+    value: 4,
+    image: skellige_berserk,
+  },
+  {
+    fraction: 'skellige',
+    cost: 8,
+    value: 6,
+    image: skellige_brideofsea,
+  },
+  {
+    fraction: 'skellige',
+    cost: 3,
+    value: 2,
+    image: skellige_crow,
+  },
   {
     fraction: 'skellige',
     cost: 9,
+    value: 7,
+    image: skellige_drakkar,
+  },
+  {
+    fraction: 'skellige',
+    cost: 10,
+    value: 8,
+    image: skellige_druid,
+  },
+  {
+    fraction: 'skellige',
+    cost: 6,
     value: 5,
-    image: skellige_warrior,
+    image: skellige_drummond,
+  },
+  {
+    fraction: 'skellige',
+    cost: 12,
+    value: 9,
+    image: skellige_hemdal,
+  },
+  {
+    fraction: 'skellige',
+    cost: 11,
+    value: 8,
+    image: skellige_hyalmar,
+  },
+  {
+    fraction: 'skellige',
+    cost: 9,
+    value: 6,
+    image: skellige_keris,
+  },
+  {
+    fraction: 'skellige',
+    cost: 7,
+    value: 5,
+    image: skellige_koral,
+  },
+  {
+    fraction: 'skellige',
+    cost: 8,
+    value: 6,
+    image: skellige_lugos,
+  },
+  {
+    fraction: 'skellige',
+    cost: 14,
+    value: 10,
+    image: skellige_melluzine,
+  },
+  {
+    fraction: 'skellige',
+    cost: 10,
+    value: 8,
+    image: skellige_svalblod,
+  },
+  {
+    fraction: 'skellige',
+    cost: 7,
+    value: 5,
+    image: skellige_svalblodwarrior,
+  },
+  {
+    fraction: 'skellige',
+    cost: 5,
+    value: 4,
+    image: skellige_threeeyes,
+  },
+  {
+    fraction: 'skellige',
+    cost: 11,
+    value: 9,
+    image: skellige_tiershach,
+  },
+  {
+    fraction: 'skellige',
+    cost: 13,
+    value: 10,
+    image: skellige_tyr,
+  },
+];
+
+export const monsterCards: Card[] = [
+  {
+    fraction: 'monster',
+    cost: 3,
+    value: 2,
+    image: monster_alp,
+  },
+  {
+    fraction: 'monster',
+    cost: 8,
+    value: 7,
+    image: monster_arianna,
+  },
+  {
+    fraction: 'monster',
+    cost: 10,
+    value: 9,
+    image: monster_auberon,
+  },
+  {
+    fraction: 'monster',
+    cost: 2,
+    value: 1,
+    image: monster_bargest,
+  },
+  {
+    fraction: 'monster',
+    cost: 12,
+    value: 10,
+    image: monster_detlaph,
+  },
+  {
+    fraction: 'monster',
+    cost: 9,
+    value: 7,
+    image: monster_eredin,
+  },
+  {
+    fraction: 'monster',
+    cost: 3,
+    value: 2,
+    image: monster_foglet,
+  },
+  {
+    fraction: 'monster',
+    cost: 11,
+    value: 9,
+    image: monster_goliafr,
+  },
+  {
+    fraction: 'monster',
+    cost: 5,
+    value: 4,
+    image: monster_griphon,
+  },
+  {
+    fraction: 'monster',
+    cost: 6,
+    value: 5,
+    image: monster_grot,
   },
   {
     fraction: 'monster',
     cost: 0,
     value: 6,
-    image: harpia,
+    image: monster_harpiya,
+  },
+  {
+    fraction: 'monster',
+    cost: 10,
+    value: 8,
+    image: monster_imlerikh,
+  },
+  {
+    fraction: 'monster',
+    cost: 8,
+    value: 6,
+    image: monster_keyran,
+  },
+  {
+    fraction: 'monster',
+    cost: 1,
+    value: 2,
+    image: monster_nacker,
+  },
+  {
+    fraction: 'monster',
+    cost: 7,
+    value: 5,
+    image: monster_nightwreith,
+  },
+  {
+    fraction: 'monster',
+    cost: 9,
+    value: 8,
+    image: monster_nithraal,
+  },
+  {
+    fraction: 'monster',
+    cost: 12,
+    value: 10,
+    image: monster_skolopendromorph,
+  },
+  {
+    fraction: 'monster',
+    cost: 10,
+    value: 9,
+    image: monster_viy,
+  },
+  {
+    fraction: 'monster',
+    cost: 4,
+    value: 3,
+    image: monster_wildhuntwarrior,
+  },
+  {
+    fraction: 'monster',
+    cost: 6,
+    value: 5,
+    image: monster_zhagnitca,
+  },
+];
+
+export const northCards: Card[] = [
+  {
+    fraction: 'north',
+    cost: 3,
+    value: 2,
+    image: north_bianka,
+  },
+  {
+    fraction: 'north',
+    cost: 5,
+    value: 4,
+    image: north_bluestripes,
+  },
+  {
+    fraction: 'north',
+    cost: 7,
+    value: 6,
+    image: north_cintraknight,
+  },
+  {
+    fraction: 'north',
+    cost: 9,
+    value: 8,
+    image: north_demavend,
+  },
+  {
+    fraction: 'north',
+    cost: 8,
+    value: 7,
+    image: north_detmold,
+  },
+  {
+    fraction: 'north',
+    cost: 4,
+    value: 3,
+    image: north_diycstra,
+  },
+  {
+    fraction: 'north',
+    cost: 6,
+    value: 5,
+    image: north_draug,
+  },
+  {
+    fraction: 'north',
+    cost: 10,
+    value: 9,
+    image: north_filippa,
+  },
+  {
+    fraction: 'north',
+    cost: 12,
+    value: 10,
+    image: north_foltest,
+  },
+  {
+    fraction: 'north',
+    cost: 5,
+    value: 4,
+    image: north_hencelt,
   },
   {
     fraction: 'north',
     cost: 2,
-    value: 8,
-    image: regis,
+    value: 1,
+    image: north_inf,
   },
+  {
+    fraction: 'north',
+    cost: 6,
+    value: 5,
+    image: north_kaedvencav,
+  },
+  {
+    fraction: 'north',
+    cost: 7,
+    value: 6,
+    image: north_meva,
+  },
+  {
+    fraction: 'north',
+    cost: 9,
+    value: 8,
+    image: north_radovid,
+  },
+  {
+    fraction: 'north',
+    cost: 4,
+    value: 3,
+    image: north_redanarch,
+  },
+  {
+    fraction: 'north',
+    cost: 8,
+    value: 7,
+    image: north_roshe,
+  },
+  {
+    fraction: 'north',
+    cost: 10,
+    value: 9,
+    image: north_shani,
+  },
+  {
+    fraction: 'north',
+    cost: 3,
+    value: 2,
+    image: north_sheala,
+  },
+  {
+    fraction: 'north',
+    cost: 11,
+    value: 10,
+    image: north_tisaya,
+  },
+  {
+    fraction: 'north',
+    cost: 7,
+    value: 6,
+    image: north_yan,
+  },
+];
+
+export const elfCard: Card[] = [
   {
     fraction: 'elf',
     cost: 5,
     value: 7,
-    image: mahakam,
+    image: elfs_mahakam,
+  },
+  {
+    fraction: 'elf',
+    cost: 6,
+    value: 8,
+    image: elfs_zoltan,
+  },
+  {
+    fraction: 'elf',
+    cost: 4,
+    value: 6,
+    image: elfs_brovven,
+  },
+  {
+    fraction: 'elf',
+    cost: 3,
+    value: 4,
+    image: elfs_dava,
+  },
+  {
+    fraction: 'elf',
+    cost: 2,
+    value: 3,
+    image: elfs_dolblatanna,
+  },
+  {
+    fraction: 'elf',
+    cost: 7,
+    value: 9,
+    image: elfs_driada,
+  },
+  {
+    fraction: 'elf',
+    cost: 5,
+    value: 6,
+    image: elfs_findabair,
+  },
+  {
+    fraction: 'elf',
+    cost: 8,
+    value: 10,
+    image: elfs_milva,
+  },
+  {
+    fraction: 'elf',
+    cost: 4,
+    value: 5,
+    image: elfs_oackritters,
+  },
+  {
+    fraction: 'elf',
+    cost: 9,
+    value: 11,
+    image: elfs_saskiya,
+  },
+  {
+    fraction: 'elf',
+    cost: 6,
+    value: 7,
+    image: elfs_sheldon,
+  },
+  {
+    fraction: 'elf',
+    cost: 7,
+    value: 8,
+    image: elfs_shirru,
+  },
+  {
+    fraction: 'elf',
+    cost: 3,
+    value: 4,
+    image: elfs_skrimmeyster,
+  },
+  {
+    fraction: 'elf',
+    cost: 2,
+    value: 3,
+    image: elfs_sukkub,
+  },
+  {
+    fraction: 'elf',
+    cost: 5,
+    value: 6,
+    image: elfs_swordmaster,
+  },
+  {
+    fraction: 'elf',
+    cost: 8,
+    value: 9,
+    image: elfs_vernosiell,
+  },
+  {
+    fraction: 'elf',
+    cost: 1,
+    value: 2,
+    image: elfs_volounteer,
+  },
+  {
+    fraction: 'elf',
+    cost: 4,
+    value: 5,
+    image: elfs_vrihhed,
+  },
+  {
+    fraction: 'elf',
+    cost: 6,
+    value: 7,
+    image: elfs_yarpen,
+  },
+  {
+    fraction: 'elf',
+    cost: 9,
+    value: 10,
+    image: elfs_yorvet,
+  },
+];
+
+export const nilfgaardCards: Card[] = [
+  {
+    fraction: 'nilfgaard',
+    cost: 4,
+    value: 6,
+    image: nilfgaard_alba,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 3,
+    value: 5,
+    image: nilfgaard_assire,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 5,
+    value: 8,
+    image: nilfgaard_catapult,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 6,
+    value: 9,
+    image: nilfgaard_cavalerry,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 8,
+    value: 12,
+    image: nilfgaard_damien,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 9,
+    value: 13,
+    image: nilfgaard_emgyr,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 3,
+    value: 4,
+    image: nilfgaard_fringiliya,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 7,
+    value: 10,
+    image: nilfgaard_impera,
   },
   {
     fraction: 'nilfgaard',
     cost: 2,
-    value: 17,
-    image: tissaya,
+    value: 3,
+    image: nilfgaard_inf,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 5,
+    value: 7,
+    image: nilfgaard_killer,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 6,
+    value: 9,
+    image: nilfgaard_peyrak,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 4,
+    value: 5,
+    image: nilfgaard_poison,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 8,
+    value: 11,
+    image: nilfgaard_riens,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 2,
+    value: 4,
+    image: nilfgaard_search,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 5,
+    value: 8,
+    image: nilfgaard_shield,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 6,
+    value: 9,
+    image: nilfgaard_skellen,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 9,
+    value: 13,
+    image: nilfgaard_strong_cav,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 7,
+    value: 10,
+    image: nilfgaard_terranova,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 3,
+    value: 4,
+    image: nilfgaard_trainer,
+  },
+  {
+    fraction: 'nilfgaard',
+    cost: 10,
+    value: 15,
+    image: nilfgaard_vilgeforc,
   },
 ];

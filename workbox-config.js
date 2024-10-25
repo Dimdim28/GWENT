d145341f414d5018c0cd821016a02a276f44c226
Dimdim28 <wwwdimanes1@gmail.com>
2024-10-25 11:31:17 +0300

    fix: added missed types to webworker

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,5 @@
 export default {
   globDirectory: 'build/',
-  globPatterns: ['**/*{css/woff2,png,svg,jpg,jpeg,js}'],
+  globPatterns: ['**/*{css/woff2,png,svg,jpg,jpeg,js,m4a,mp3}'],
   swDest: 'build/sw.js',
 };

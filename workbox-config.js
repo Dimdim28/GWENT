990b0c0a157cf33b0fbe77fdbad3a2da5bd78359
Dimdim28 <wwwdimanes1@gmail.com>
2024-10-25 11:43:49 +0300

    feat: improved caching rules

--- workbox-config.js
+++ workbox-config.js
@@ -2,4 +2,16 @@ export default {
   globDirectory: 'build/',
   globPatterns: ['**/*.{css,woff2,png,svg,jpg,jpeg,js,m4a,mp3}'],
   swDest: 'build/sw.js',
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|m4a|mp3)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images-and-audio',
+        expiration: {
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        },
+      },
+    },
+  ],
 };
